8bb98518add0e44a2e606628e1e109d86bfd8bad
eestelle <yosiky@list.ru>
2022-02-17 18:53:26 +0300

    ...

--- src/client/signal.c
+++ src/client/signal.c
@@ -1,31 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   signal.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: eestelle </var/spool/mail/eestelle>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/02/17 18:11:45 by eestelle          #+#    #+#             */
-/*   Updated: 2022/02/17 18:49:58 by eestelle         ###   ########.fr       */
+/*   Created: 2022/02/17 18:46:11 by eestelle          #+#    #+#             */
+/*   Updated: 2022/02/17 18:46:33 by eestelle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "client.h"
 
-int	main(int argc, char **argv)
+void	ft_init_signal(void)
 {
-	pid_t	pid;
 
-	if (argc == 3)
-	{
-		pid = ft_atoi(argv[1]);
-		ft_init_signals();
-		if (kill(pid, SIGUSR1) < 0)
-			write(2, "Error: kill\n", 12);
-		while (1)
-			pause();
-	}
-	else
-		ft_putstr_fd("./client [PID Server] \"Your string\"\n", 1);
-	return (0);
+
 }
